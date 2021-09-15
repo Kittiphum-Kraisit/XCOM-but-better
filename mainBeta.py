cc727cee3fff769f79c2780cfbbc73f7a6fcd704
Kittiphum-Kraisit <63011175@kmitl.ac.th>
2021-09-15 17:28:22 +0700

    Version 2

--- mainBeta.py
+++ mainBeta.py
@@ -23,13 +23,15 @@ white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 
+
 #images
 background_img = pygame.image.load('pic/black.jpg').convert_alpha()
+
 
 #pygame object
 class char_card:
     def __init__(self, x, y, id):
-        self.id = id;
+        self.id = id
         img = pygame.image.load('pic/cat.png')
         self.image = pygame.transform.scale(img, (img.get_width()//3, img.get_height()//3))
         self.x = x
@@ -40,28 +42,39 @@ class char_card:
 
     def draw(self):
         screen.blit(self.image, self.rect)
-        info = obj_char_create(str(self.id),0)
-        draw_text(info.Name, font, white, self.x - 50, self.y+40)
+        # info = obj_char_create(str(self.id),0)
+        # draw_text(info.Name, font, white, self.x - 50, self.y+40)
+        # draw_text(f'HP: {str(info.HP)}', font, white, self.x - 50, self.y + 60)
+        # draw_text(f'Mana: {str(info.Mana)}', font, white, self.x - 50, self.y + 80)
+        # draw_text(f'Skill: {info.Skill_name}', font, white, self.x - 50, self.y + 100)
+        # draw_text(f'AtkDmg: {str(info.Atk_damage)}', font, white, self.x - 50, self.y + 120)
+
+    def draw_t(self):
+        info = obj_char_create(str(self.id), 0)
+        draw_text(info.Name, font, white, self.x - 50, self.y + 40)
         draw_text(f'HP: {str(info.HP)}', font, white, self.x - 50, self.y + 60)
         draw_text(f'Mana: {str(info.Mana)}', font, white, self.x - 50, self.y + 80)
         draw_text(f'Skill: {info.Skill_name}', font, white, self.x - 50, self.y + 100)
-        draw_text(f'AtkDmg{str(info.Atk_damage)}', font, white, self.x - 50, self.y + 120)
+        draw_text(f'AtkDmg: {str(info.Atk_damage)}', font, white, self.x - 50, self.y + 120)
 
 
-
-#pygame functions
+# pygame functions
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col, (0,0,0))
     screen.blit(img, (x, y))
 
+
 def draw_bg():
     screen.blit(background_img, (0, 0))
 
+
 c = []
-for i in range (0,5):
+cc_team1 = []
+cc_team2 = []
+for i in range(0, 5):
      c.append(char_card(90 + (150*i), 100, i))
 
-for i in range (4,10):
+for i in range(4, 10):
      c.append(char_card(90 + (150*(i-4)), 300, i))
 
 
@@ -69,7 +82,6 @@ run = True
 scene = 1
 while run:
     screen.fill(0)
-
     if scene == 1:
         draw_bg()
         draw_text("Welcome", font, white, 350, 200)
@@ -77,19 +89,30 @@ while run:
     elif scene == 2:
         draw_bg()
         for i in range(10):
-            c[i].draw()
+            if c[i].available:
+                c[i].draw()
+            elif not c[i].available:
+                pygame.draw.rect(screen, (196, 196, 196, 25), c[i].rect)
+                c[i].draw()
+            c[i].draw_t()
         draw_text("Select character", font, white, 370, 0)
 
-
     for event in pygame.event.get():
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             scene = 2
+            if scene == 2:
+                pos = pygame.mouse.get_pos()
+                for i in range(0, 10):
+                    if c[i].rect.collidepoint(pos):
+                        if c[i].available and (len(cc_team1) < 5):
+                            cc_team1.append(c[i].id)
+                            c[i].available = not c[i].available
+                        elif not c[i].available and c[i].id in cc_team1:
+                            cc_team1.remove(c[i].id)
+                            c[i].available = not c[i].available
+                        print(cc_team1)
         if event.type == pygame.QUIT:
             run = False
-
-
     pygame.display.update()
-
 pygame.quit()
 
